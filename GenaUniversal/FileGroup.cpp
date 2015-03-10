6962fba2631dc70c0962129fc2324784a87690b9
gaotianyu1350 <gaotianyu1350@126.com>
2015-03-10 19:58:04 +0800

    FileGroup

---
+++ GenaUniversal/FileGroup.cpp
@@ -0,0 +1,10 @@
+/***************************************************************
+ * Name:      FileGroup.cpp
+ * Purpose:   Code for File Group Class
+ * Author:    VL (Vincent Gao, Liangjs)
+ * Created:   2015-03-10
+ * Copyright: VL (Vincent Gao, Liangjs)
+ **************************************************************/
+
+#include "FileGroup.h"
+
---
+++ GenaUniversal/FileGroup.h
@@ -0,0 +1,14 @@
+/***************************************************************
+ * Name:      FileGroup.h
+ * Purpose:   Defines File Group Class
+ * Author:    VL (Vincent Gao, Liangjs)
+ * Created:   2015-03-10
+ * Copyright: VL (Vincent Gao, Liangjs)
+ **************************************************************/
+
+#ifndef FILEGROUP_H_INCLUDED
+#define FILEGROUP_H_INCLUDED
+
+
+
+#endif // FILEGROUP_H_INCLUDED
