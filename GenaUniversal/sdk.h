29c0d2c08f9258a7221e3591d27fc93c4a86654c
liangjs <761232680@qq.com>
2015-03-12 10:00:23 +0800

    sdk

---
+++ GenaUniversal/sdk.h
@@ -0,0 +1,11 @@
+#ifndef SDK_H
+#define SDK_H
+
+#include <bits/stdc++.h>
+#include "Library.h"
+#include "FileGroup.h"
+#include "File.h"
+#include "FileManager.h"
+#include "TempFile.h"
+
+#endif // SDK_H
