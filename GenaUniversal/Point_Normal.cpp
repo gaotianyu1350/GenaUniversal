2e29ec8b8e56a8894fe8f1eaf73c421c4234bf2c
liangjs <761232680@qq.com>
2015-03-13 21:14:34 +0800

    normal

--- GenaUniversal/Point_Normal.cpp
+++ GenaUniversal/Point_Normal.cpp
@@ -4,5 +4,27 @@ using namespace std;
 
 extern "C"
 {
+    class Point_Normal : public Point
+    {
+    public:
+        Point_Normal(const bool *flag, qMs *queueMessage, FileGroup *fg)
+            : Point(flag, queueMessage, fg)
+        {
+        }
 
+        virtual void run()
+        {
+
+        }
+
+        virtual void onStop()
+        {
+            Point::onStop();
+        }
+    };
+
+    Point_Normal *get(const bool *flag, qMs *queueMessage, FileGroup *fg)
+    {
+        return new Point_Normal(flag, queueMessage, fg);
+    }
 }
