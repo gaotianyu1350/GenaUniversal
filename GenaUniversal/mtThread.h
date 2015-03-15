36521972846d2642f8b0bd8cbb5efb25715457bd
gaotianyu1350 <gaotianyu1350@126.com>
2015-03-15 22:15:35 +0800

    doubi

---
+++ GenaUniversal/mtThread.h
@@ -0,0 +1,33 @@
+#ifndef MTTHREAD_H
+#define MTTHREAD_H
+
+#include "sdk.h"
+
+#ifdef WIN32
+#include <windows.h>
+#include <process.h>
+#else
+#endif
+
+typedef (void (*)(void *)) functionType;
+
+class mtThread
+{
+//#ifdef WIN32
+public:
+    mtThread(functionType func, void *arg)
+    ~mtThread()
+
+    DWORD getExitCode()
+    int isActive()
+    void kill()
+
+private:
+    HANDLE pThread;
+//#else
+
+//#endif
+
+};
+
+#endif // MTTHREAD_H
