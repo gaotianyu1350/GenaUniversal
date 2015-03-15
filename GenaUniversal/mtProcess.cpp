44df0c64a9f78f35e00dc6faa11a4f599315c540
gaotianyu1350 <gaotianyu1350@126.com>
2015-03-15 21:57:39 +0800

    finish process add compile

--- GenaUniversal/mtProcess.cpp
+++ GenaUniversal/mtProcess.cpp
@@ -1,2 +1,30 @@
 #include "mtProcess.h"
 
+mtProcess::mtProcess(functionType func, void *arg)
+{
+    pThread = (HANDLE)_beginthread(fc, 0, arg);
+}
+
+mtProcess::~mtProcess()
+{
+    kill();
+}
+
+DWORD mtProcess::getExitCode()
+{
+    DWORD f;
+    GetExitCodeProcess(hProcess, &f);
+    return f;
+}
+
+int mtProcess::isActive()
+{
+    return getExitCode() == STILL_ACTIVE;
+}
+
+void mtProcess::kill()
+{
+    if (isActive())
+        TerminateProcess(hProcess, 4);
+    _endthread();
+}
--- GenaUniversal/mtProcess.h
+++ GenaUniversal/mtProcess.h
@@ -3,13 +3,27 @@
 
 #include "sdk.h"
 
+#ifdef WIN32
+#include <windows.h>
+#include <process.h>
+#else
+#endif
+
+typedef (void (*)(void *)) functionType;
+
 class mtProcess
 {
 //#ifdef WIN32
 public:
+    mtProcess(functionType func, void *arg)
+    ~mtProcess()
+
+    DWORD getExitCode()
+    int isActive()
+    void kill()
 
 private:
-
+    HANDLE pThread;
 //#else
 
 //#endif
