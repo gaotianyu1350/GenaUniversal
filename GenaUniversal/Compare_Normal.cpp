cde7f6cc32729c77c94015cc080cf73db224981d
kzoacn <kzoacn@outlook.com>
2015-03-16 08:33:30 +0800

    zky_03-16 8:33

--- GenaUniversal/Compare_Normal.cpp
+++ GenaUniversal/Compare_Normal.cpp
@@ -11,34 +11,87 @@ extern "C"
             : Compare(flag, queueMessage, setting, result)
         {
         }
+        void setFile(const string _OutFile, const string _AnsFile)
+        {
+            AnsFile = _AnsFile;
+            OutFile = _OutFile;
+        }
         virtual void run()
         {
-            /*Setting *data = setting->getItem("data");
-            Setting *_player = setting->getItem("player");
-            map<string, Setting_data> &players = _player->getAllItems();
-            Library lib("Player_Normal.dll");
-            typedef Player *(*GET)(const bool*, qMs*, Setting*, Result*);
-            GET getPlayer = (GET)lib.get("get");
-            for (map<string, Setting_data>::iterator i = players.begin(); i != players.end(); ++i)
+            ifstream fans(AnsFile.c_str());
+            ifstream fout(OutFile.c_str());
+            string tmpstr;
+            vector<string>vecans, vecout;
+            if (fans == 0)
             {
-                if (isStop())
-                {
-                    onStop();
-                    return;
-                }
-                ((Setting*)(i->second))->setItem("data", data);
-                Result *res = new Result(i->first);
-                Player *player = getPlayer(flagStop, queueMessage, i->second, res);
-                player->run();
-                delete player;
-                result->setItem(i->first, res);
+                pushMessage(1, "Can not open answer file");
+                return ;
             }
-            pushMessage(0, "Contest judgement finished");*/
+            if (fout == 0)
+            {
+                pushMessage(1, "Can not open output file");
+                return ;
+            }
+            result->setItem("compare", "Accepted");
+            while (getline(fans, tmpstr))
+            {
+                vecans.push_back(tmpstr);
+            }
+            while (getline(fout, tmpstr))
+            {
+                vecout.push_back(tmpstr);
+            }
+            if (vecans.size() > vecout.size())
+            {
+                sprintf(str, "%d", int(vecout.size()));
+                result->setItem("compare", strcat("Wrong_Answer at ", str));
+            }
+            else if (vecans.size() < vecout.size())
+            {
+                sprintf(str, "%d", int(vecans.size()));
+                result->setItem("compare", strcat("Wrong_Answer at ", str));
+            }
+            else
+            {
+                for (int i = 0 ; i < vecout.size() ; i++)
+                {
+                    if (vecans[i] != vecout[i])
+                    {
+                        sprintf(str, "%d", i + 1);
+                        result->setItem("compare", strcat("Wrong_Answer at ", str));
+                        break;
+                    }
+                }
+            }
+            vecans.clear();
+            vecout.clear();
+            fans.close();
+            fout.close();
+            fans.clear();
+            fout.clear();
+            fout.open(OutFile.c_str());
+            fans.open(AnsFile.c_str());
+            while(fans>>tmpstr)
+            {
+                vecans.push_back(tmpstr);
+            }
+            while(fout>>tmpstr)
+            {
+                vecout.push_back(tmpstr);
+            }
+            if(vecans==vecout))
+            {
+                result->setItem("compare", "Presentation_Error");
+            }
+            pushMessage(0, "Compare finished");
         }
         virtual void onStop()
         {
             Compare::onStop();
         }
+    private:
+        string AnsFile, OutFile;
+        char str[10];
     };
     Compare_Normal *get(const bool *flag, qMs *queueMessage, Setting *setting, Result *result)
     {
