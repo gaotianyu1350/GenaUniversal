f1b9bcadd9d08785d56ad3078fdd6def94bb7d8d
liangjs <761232680@qq.com>
2015-03-17 21:38:32 +0800

    tree

--- GenaUniversal/SettingTreeFrame.h
+++ GenaUniversal/SettingTreeFrame.h
@@ -3,17 +3,32 @@
 
 #include <wx/wx.h>
 #include "Setting.h"
+#include <wx/glcanvas.h>
 #include <GL/glu.h>
+
+class SettingGLCanvas : public wxGLCanvas
+{
+public:
+    SettingGLCanvas(wxWindow *parent, wxWindowID id = wxID_ANY, const wxString &name = wxEmptyString);
+    void OnPaint(wxPaintEvent &event);
+    void OnMouse(wxMouseEvent &event);
+private:
+    wxGLContext *glRC;
+    DECLARE_EVENT_TABLE()
+};
 
 class SettingTreeFrame: public wxFrame
 {
 public:
-    SettingTreeFrame(wxWindow *parent, wxWindowID winid, Setting *init = NULL, const wxString &name = wxEmptyString);
-    virtual ~SettingTreeFrame();
+    SettingTreeFrame(wxWindow *parent, wxWindowID winid = wxID_ANY, Setting *init = NULL, const wxString &name = wxEmptyString);
+    void clickOK(wxCommandEvent &event);
+    void clickCancel(wxCommandEvent &event);
 private:
-    Setting *data;
-    wxButton *btnOK, *btnCancel;
+    wxPanel *panel;
     wxBoxSizer *topSizer, *btnSizer;
+    wxButton *btnOK, *btnCancel;
+    SettingGLCanvas *canvas;
+    Setting *data;
     DECLARE_EVENT_TABLE()
 };
 
