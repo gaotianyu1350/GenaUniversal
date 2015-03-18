2e1a14429099b19d1ffb9f1c1202511953bf7d9b
gaotianyu1350 <gaotianyu1350@126.com>
2015-03-18 20:12:45 +0800

    add compile_normal

--- GenaUniversal/Compile.h
+++ GenaUniversal/Compile.h
@@ -6,6 +6,13 @@
 class Compile : public sdk
 {
 public:
+    static const int COMPILE_RES_NULL        = 0;
+    static const int COMPILE_RES_NO_CODE     = 1;
+    static const int COMPILE_RES_NO_COMPILER = 2;
+    static const int COMPILE_RES_CE          = 2;
+    static const int COMPILE_RES_OK          = 3;
+    static const int COMPILE_RES_TER         = 4;
+
     Compile(const bool *flag, qMs *queueMessage, Setting *setting, Result *result);
     ~Compile();
 
@@ -17,6 +24,8 @@ public:
 *
 * Setting :
 *     "code" : the file that need compiling.
+*     ¡°setting" : the settings about compiling.
+*         (example) "cpp" : "g++ ${src} -o ${exe}"
 *
 * Result :
 *     "exe" : the file got after compiling.
--- GenaUniversal/Compile_Normal.cpp
+++ GenaUniversal/Compile_Normal.cpp
@@ -12,6 +12,24 @@ extern "C"
 
         virtual void run()
         {
+            if (!setting->hasItem("code"))
+            {
+                setResult(Compile::COMPILE_RES_NO_CODE, "Hasn't set the code. ");
+                return;
+            }
+
+            std::string code = setting->getItem("code").operator std::string &();
+            if (!FileManager::isfile(code))
+            {
+                setResult(Compile::COMPILE_RES_NO_CODE, "There is no code. ");
+                return;
+            }
+
+            std::string ext = FileManager::getext(code);
+            for (std::string::iterator p = ext.begin(); p != ext.end(); p++)
+                if ('A' <= *p && *p <= 'Z')
+                    *p = *p - 'A' + 'a';
+
 
         }
 
@@ -21,6 +39,10 @@ extern "C"
         }
 
     private:
-
+        void setResult(int resultCode, const std::string &detail)
+        {
+            result->setItem("result", resultCode);
+            result->setItem("detail", detail);
+        }
     };
 }
