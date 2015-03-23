fbcc99e80d76ae116aabc1c64fe6e78d8632a3b0
liangjs <761232680@qq.com>
2015-03-23 18:35:44 +0800

    doubi

--- GenaUniversal/TempFile.cpp
+++ GenaUniversal/TempFile.cpp
@@ -1,16 +1,26 @@
+/***************************************************************
+ * Name:      TempFile.cpp
+ * Purpose:   Code for TempFile Class
+ * Author:    VL (Vincent Gao, Liangjs)
+ * Created:   2015-03-11
+ * Copyright: VL (Vincent Gao, Liangjs)
+ **************************************************************/
+
 #include "TempFile.h"
 #include "FileManager.h"
 #include <cstdlib>
 #include <ctime>
 #include <unistd.h>
 #include <set>
-#include <sys/stat.h>
-#include <sys/types.h>
 
 void TempFile::InitTempFile()
 {
     ClearTempDir();
+#ifdef WIN32
     mkdir("tmp");
+#else
+    system("mkdir -p tmp");
+#endif
     srand(time(0));
 }
 
@@ -31,8 +41,7 @@ std::string TempFile::GetTempFile(const std::string &prefix, const std::string &
         for (int i = 0; i < 10; ++i)
             ans += charrec[rand() % (26 * 2 + 10)];
         ans += suffix;
-    }
-    while (!access(ans.c_str(), F_OK) || use.find(ans) != use.end());
+    } while (!access(ans.c_str(), F_OK) || use.find(ans) != use.end());
     use.insert(ans);
     return ans;
 }
